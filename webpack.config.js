7a7683fdad445cd37015ba54134cc7205283ae85
pzinn <pzinnjustin@gmail.com>
2020-04-22 11:38:40 +1000

    update config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
-var webpack = require("webpack");
-
 module.exports = {
   entry: "./public-source/index.js",
   output: {
-    path: __dirname,
     filename: "public/public-common/index.js"
   },
   mode: 'production',
-  module: {
-  },
-  plugins: [
-  ],
-  devtool: "inline-module-source-map"
 };
--- webpack.debug-config.js
+++ webpack.debug-config.js
@@ -1,15 +1,8 @@
-var webpack = require("webpack");
-
 module.exports = {
   entry: "./public-source/index.js",
   output: {
-    path: __dirname,
     filename: "public/public-common/index.js"
   },
   mode: 'development',
-  module: {
-  },
-  plugins: [
-  ],
   devtool: "inline-module-source-map"
 };
