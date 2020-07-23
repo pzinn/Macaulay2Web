ee6ad242ab2d917dd384393ca58648113c77974f
Paul Zinn-Justin <pzinnjustin@gmail.com>
2020-07-23 17:34:37 +1000

    fix cookie cross site issue

--- src/server/server.ts
+++ src/server/server.ts
@@ -606,6 +606,8 @@ const setCookieOnSocket = function (socket, clientID: string): void {
   const expDate = new Date(new Date().getTime() + sevenDays);
   const sessionCookie = Cookie.serialize(options.cookieName, clientID, {
     expires: expDate,
+    sameSite: "none",
+    secure: true,
   });
   socket.emit("cookie", sessionCookie);
 };
