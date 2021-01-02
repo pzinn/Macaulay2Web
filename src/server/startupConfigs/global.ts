d62ef22263ff4742d3174a894ae7170b9eecd860
pzinn <pzinnjustin@gmail.com>
2021-01-02 15:00:25 -0400

    add missing global options file

---
+++ src/server/startupConfigs/global.ts
@@ -0,0 +1,13 @@
+const options = {
+  cookieName: "Macaulay2Web",
+  cookieAliasName: "Macaulay2Web_alias",
+  cookieDuration: 1000 * 60 * 60 * 24 * 31, // one month
+  defaultAlias: "Anonymous",
+  adminAlias: "Admin",
+  perContainerResources: {
+    cpuShares: 0.5,
+    memory: 384, // Mb
+  },
+};
+
+export { options };
