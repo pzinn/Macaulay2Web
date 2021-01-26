ca6550702e379250b4627924be7bcbae4d128d63
pzinn <pzinnjustin@gmail.com>
2021-01-26 14:29:14 +1000

    minor

--- src/server/server.ts
+++ src/server/server.ts
@@ -245,8 +245,13 @@ const sendDataToClient = function (client: Client) {
       while (client.output.length > options.perContainerResources.maxOutput) {
         const m = client.output.match(
           new RegExp(
-            webAppTags.Cell + "[^" + webAppTags.Cell + "]*" + webAppTags.CellEnd
-          ) // probably better syntax with *?
+            webAppTags.Cell +
+              "[^" +
+              webAppTags.Cell +
+              webAppTags.CellEnd +
+              "]*" +
+              webAppTags.CellEnd
+          ) // tricky: because of possible nesting
         );
         if (m === null) break; // give up -- normally, shouldn't happen except transitionally
         client.output =
@@ -547,7 +552,7 @@ const socketChatAction = function (socket, client: Client) {
             : "");
       }
     }
-    socket.emit("chat", chat);
+    safeSocketEmit(socket, "chat", chat);
   };
 
   return client.id != "user" + options.adminName
