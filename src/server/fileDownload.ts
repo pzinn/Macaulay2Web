008e586465d5ac37882878e6db88d526a1ee1a76
pzinn <pzinnjustin@gmail.com>
2021-04-23 13:58:39 +1000

    editor directory listing

--- src/server/fileDownload.ts
+++ src/server/fileDownload.ts
@@ -15,27 +15,12 @@ const downloadFromDocker = function (
   sshCredentials,
   next
 ) {
-  const fileName: string = path.basename(sourceFileName);
+  let fileName: string = path.basename(sourceFileName);
   if (!fileName || !client.instance || !client.instance.host) return next();
   const sshConnection: ssh2.Client = new ssh2.Client();
 
   const userPath = userSpecificPath(client);
   const targetPath = staticFolder + userPath;
-  const targetFileName = targetPath + fileName;
-  let sourceFileName1 = sourceFileName; // adjusted for possible base dir
-
-  const success = function () {
-    logClient(client, "Successfully downloaded " + sourceFileName1);
-    setTimeout(unlink(targetFileName), 1000 * 60 * 10);
-    sshConnection.end();
-    next(userPath + fileName);
-  };
-
-  const failure = function () {
-    logClient(client, "failed to download " + sourceFileName);
-    sshConnection.end();
-    next();
-  };
 
   fs.mkdir(targetPath, function (fsError) {
     if (fsError) {
@@ -44,24 +29,69 @@ const downloadFromDocker = function (
     }
     sshConnection.on("ready", function () {
       sshConnection.sftp(function (generateError, sftp) {
+        let sourceFileName1; // adjusted for possible base dir
+        let targetFileName;
+
+        const success = function () {
+          logClient(client, "Successfully downloaded " + sourceFileName1);
+          setTimeout(unlink(targetFileName), 1000 * 60 * 10);
+          sshConnection.end();
+          next(userPath + fileName);
+        };
+        const failure = function () {
+          logClient(client, "failed to download " + sourceFileName);
+          sshConnection.end();
+          next();
+        };
+
         if (generateError) {
           logger.error("ssh2.sftp() failed: " + generateError);
           return failure();
         }
+
+        const checkExists = function (doesNot) {
+          sftp.stat(sourceFileName1, function (err, res) {
+            if (err || !res) doesNot();
+            else if (res.isFile()) copyFile();
+            else if (res.isDirectory()) readDir();
+            else doesNot(); // we don't follow symbolic links
+          });
+        };
+
+        const copyFile = function () {
+          targetFileName = targetPath + fileName;
+          sftp.fastGet(sourceFileName1, targetFileName, function (sftpError) {
+            if (!sftpError) success();
+            else failure();
+          });
+        };
+
+        const readDir = function () {
+          sftp.readdir(sourceFileName1, function (sftpError, lst) {
+            if (sftpError) return failure();
+            const s = lst
+              .map((x) => x.filename + (x.longname[0] == "d" ? "/" : ""))
+              .join("\n");
+            fileName = "directory@" + fileName; // eww
+            targetFileName = targetPath + fileName;
+            fs.writeFile(targetFileName, s, function (sftpError) {
+              if (!sftpError) success();
+              else failure();
+            });
+          });
+        };
+
+        // determine where and if file exists
         const relative = !sourceFileName.startsWith("/");
         sourceFileName1 =
           options.serverConfig.baseDirectory +
           (relative ? sourceFileName : sourceFileName.substring(1));
-        sftp.fastGet(sourceFileName1, targetFileName, function (sftpError) {
-          if (!sftpError) success();
-          else if (relative) failure();
+        checkExists(function () {
+          if (relative) failure();
           else {
             // annoying subtlety: if relative false, we don't know if path relative or absolute => try both :/
             sourceFileName1 = sourceFileName;
-            sftp.fastGet(sourceFileName1, targetFileName, function (sftpError) {
-              if (!sftpError) success();
-              else failure();
-            });
+            checkExists(failure);
           }
         });
       });
