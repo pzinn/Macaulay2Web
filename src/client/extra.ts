7536eba096d4bbb597e5670c369ecba892ee0d14
pzinn <pzinnjustin@gmail.com>
2021-05-04 22:43:27 +1000

    fix tutorial bug

--- src/client/extra.ts
+++ src/client/extra.ts
@@ -9,11 +9,7 @@ import {
   nextChar,
 } from "./htmlTools";
 import { socketChat, syncChat } from "./chat";
-import {
-  initTutorials,
-  uploadTutorial,
-  displayLessonIfChanged,
-} from "./tutorials";
+import { initTutorials, uploadTutorial, displayLesson } from "./tutorials";
 import { Chat } from "../common/chatClass";
 import {
   escapeKeyHandling,
@@ -278,9 +274,9 @@ const extra1 = function () {
 
   let tab = url.hash;
   const m = /^#tutorial-(\w+)(?:-(\d+))?$/.exec(tab);
-  const tute = m ? m[1] : null,
+  const tute = m ? m[1] : "0",
     page = m && m[2] ? +m[2] : 1;
-  initTutorials(tute, page - 1);
+  initTutorials(tute, page);
   const upTutorial = document.getElementById("uptutorial");
   if (upTutorial) {
     upTutorial.onchange = uploadTutorial;
@@ -293,7 +289,7 @@ const extra1 = function () {
     const m = /^tutorial(?:-(\w+))?(?:-(\d+))?$/.exec(loc);
     if (m) {
       loc = "tutorial";
-      displayLessonIfChanged(m[1] || 0, (+m[2] || 1) - 1);
+      displayLesson(m[1], m[2]);
     }
     // editor stuff
     const e = /^editor:(.+)$/.exec(loc);
