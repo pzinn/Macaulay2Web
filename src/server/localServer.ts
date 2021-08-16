b1610394f8fcec66b609d79eb8a80c3c33a60f16
pzinn <pzinnjustin@gmail.com>
2021-08-17 01:56:29 +1000

    turning off echo of terminal

--- src/server/localServer.ts
+++ src/server/localServer.ts
@@ -45,7 +45,7 @@ const options = {
       baseDirectory +
       "; cd " +
       baseDirectory +
-      "; TERM=dumb M2MODE=localServer M2 --webapp --no-readline",
+      "; TERM=dumb M2MODE=localServer stty -echo; M2 --webapp",
     CONTAINERS(): InstanceManager {
       return new LocalContainerManager();
     },
