1a536f6b2ac6b9eadc3e05c81dba58ddd4d9c4c1
pzinn <pzinnjustin@gmail.com>
2021-09-20 13:46:52 +1000

    improved regex handling in conversion from md

--- src/client/md.ts
+++ src/client/md.ts
@@ -13,6 +13,67 @@ const escapeHTML = (
         '"': "&quot;",
       }[tag])
   );
+
+const cut = (s, x) => mdReplace(s.substring(x.index + x[0].length));
+// (?<![^\s>]+\s*) means: should be at start of line except possible spaces and ~ html tags
+
+// simple substitutions
+// prettier-ignore
+const mdSubst = [ // ["!\\[([^\\]]*)]\\(([^(]+)\\)",'<img alt="$1" src="$2">'],
+    { pattern: "\\[([^\\]]+)]\\(([^(]+?)\\)", subst: "<a href='$2'>$1</a>"}, // [a link](https://github.com)
+    { pattern: "`([^`]*)`", subst: "<code class='inline'>$1</code>"}, // `R=QQ[x]`
+    { pattern: "(?<!\\S)\\*\\*(?=\\S)([^\\r]*?\\S)(?<!\\\\)\\*\\*", subst: "<strong>$1</strong>"}, // **really important**
+    { pattern: "\\b__(?=\\S)([^\\r]*?\\S)(?<!\\\\)__", // we can use \\b because _ is considered part of the word
+      subst: "<u><strong>$1</strong></u>"}, // __really important__
+    { pattern: "(?<!\\S)\\*(?!\\s|\\*)([^\\r]*?\\S)(?<!\\\\)\\*", subst: "<em>$1</em>"}, // *important*
+    { pattern: "\\b_(?!\\s|_)([^\\r]*?\\S)(?<!\\\\)_", subst: "<u>$1</u>"}, // _underlined_
+    { pattern: "\\\\n", subst: "<br/>"}] as any; // "\n" for nonsplitting newline
+
+// more complex patterns
+const patterns = [
+  {
+    pattern: "(?<![^\\s>]+\\s*)\\*\\s",
+    tag: "ul",
+    linetag: () => "li",
+    proc: cut,
+  },
+  {
+    pattern: "(?<![^\\s>]+\\s*)\\d+\\.\\s",
+    tag: "ol",
+    linetag: () => "li",
+    proc: cut,
+  },
+  {
+    pattern: "(?<![^\\s>]+\\s*)#+\\s",
+    tag: null,
+    linetag: (x) => "h" + (x[0].length - 1),
+    proc: cut,
+  },
+  {
+    pattern: "(?<![^\\s>]+\\s*)\\|",
+    tag: "table",
+    linetag: () => "tr",
+    proc: (s, x) => {
+      s = cut(s, x);
+      const m = s.match(/\|\s*$/);
+      if (m) s = s.substring(0, m.index);
+      return "<td>" + mdReplace(s).replace(/\|/g, "</td><td>") + "</td>";
+    },
+  },
+  {
+    pattern: "(?<![^\\s>]+\\s*)```sh",
+    tag: "pre",
+    linetag: null,
+    proc: (s, x) => (x === null ? s + "\n" : ""),
+  },
+  {
+    pattern: "(?<![^\\s>]+\\s*)```", // really, this should be ```m2 and previous one should be ```
+    tag: "code",
+    linetag: null,
+    proc: (s, x) => (x === null ? s + "\n" : ""),
+  },
+] as any;
+
 const forbiddenTags = [
   "script",
   "base",
@@ -24,78 +85,39 @@ const forbiddenTags = [
   "frame",
   "html",
   "iframe",
-  "nav",
   "object",
 ];
-const forbiddenTagsRegex = new RegExp(
-  forbiddenTags.map((tag) => "(?<=<|</)" + tag).join("|") +
-    "|(?<=<[^>]*\\s)id=|(?<=<[^>]*\\s)for=|(?<=<[^>]*)mdl-",
-  "g"
-);
-const removeForbiddenTags = (str) => str.replace(forbiddenTagsRegex, "no$&");
+let forbiddenTagsRegex;
+let removeForbiddenTags;
+try {
+  for (let j = 0; j < mdSubst.length; j++)
+    mdSubst[j].pattern = new RegExp(mdSubst[j].pattern, "g");
+  for (let j = 0; j < patterns.length; j++)
+    patterns[j].pattern = new RegExp(patterns[j].pattern);
+  forbiddenTagsRegex = new RegExp(
+    forbiddenTags.map((tag) => "(?<=<|</)" + tag).join("|") +
+      "|(?<=<[^>]*\\s)id=|(?<=<[^>]*\\s)for=|(?<=<[^>]*)mdl-",
+    "g"
+  );
+  removeForbiddenTags = (str) => str.replace(forbiddenTagsRegex, "no$&");
+} catch (err) {
+  console.log("regex failure"); // e.g. with safari
+  mdSubst.length = 0;
+  patterns.length = 0;
+  forbiddenTagsRegex = new RegExp(
+    forbiddenTags.map((tag) => "<" + tag).join("|"),
+    "g"
+  );
+  removeForbiddenTags = (str) => str.replace(forbiddenTagsRegex, ""); // TEMP
+}
 
 const mdReplace = function (str: string) {
   const pieces = str.split("\t"); // \t to prevent any markdown changes
   for (let i = 0; i < pieces.length; i += 2)
-    pieces[i] = pieces[i]
-      /*      .replace(/!\[([^\]]*)]\(([^(]+)\)/g, '<img alt="$1" src="$2">') */
-      .replace(/\[([^\]]+)]\(([^(]+?)\)/g, "<a href='$2'>$1</a>")
-      // [a link](https://github.com)
-      .replace(/`([^`]*)`/g, "<code class='inline'>$1</code>") // `R=QQ[x]`
-      .replace(
-        /(?<!\S)\*\*(?=\S)([^\r]*?\S)(?<!\\)\*\*/g,
-        "<strong>$1</strong>"
-      ) // **really important**
-      .replace(
-        /\b__(?=\S)([^\r]*?\S)(?<!\\)__/g, // we can use \b because _ is considered part of the word
-        "<u><strong>$1</strong></u>"
-      ) // __really important__
-      .replace(/(?<!\S)\*(?!\s|\*)([^\r]*?\S)(?<!\\)\*/g, "<em>$1</em>") // *important*
-      .replace(/\b_(?!\s|_)([^\r]*?\S)(?<!\\)_/g, "<u>$1</u>") // _underlined_
-      .replace(/\\n/g, "<br/>"); // "\n" for nonsplitting newline
+    for (let j = 0; j < mdSubst.length; j++)
+      pieces[i] = pieces[i].replace(mdSubst[j].pattern, mdSubst[j].subst);
   return pieces.join("\t");
 };
-
-const cut = (s, x) => mdReplace(s.substring(x.index + x[0].length));
-// (?<![^\s>]+\s*) means: should be at start of line except possible spaces and ~ html tags
-const patterns = [
-  { pattern: /(?<![^\s>]+\s*)\*\s/, tag: "ul", linetag: () => "li", proc: cut },
-  {
-    pattern: /(?<![^\s>]+\s*)\d+\.\s/,
-    tag: "ol",
-    linetag: () => "li",
-    proc: cut,
-  },
-  {
-    pattern: /(?<![^\s>]+\s*)#+\s/,
-    tag: null,
-    linetag: (x) => "h" + (x[0].length - 1),
-    proc: cut,
-  },
-  {
-    pattern: /(?<![^\s>]+\s*)\|/,
-    tag: "table",
-    linetag: () => "tr",
-    proc: (s, x) => {
-      s = cut(s, x);
-      const m = s.match(/\|\s*$/);
-      if (m) s = s.substring(0, m.index);
-      return "<td>" + mdReplace(s).replace(/\|/g, "</td><td>") + "</td>";
-    },
-  },
-  {
-    pattern: /(?<![^\s>]+\s*)```sh/,
-    tag: "pre",
-    linetag: null,
-    proc: (s, x) => (x === null ? s + "\n" : ""),
-  },
-  {
-    pattern: /(?<![^\s>]+\s*)```/, // really, this should be ```m2 and previous one should be ```
-    tag: "code",
-    linetag: null,
-    proc: (s, x) => (x === null ? s + "\n" : ""),
-  },
-];
 
 const mdToHTML = function (src, sep, doublesep) {
   // e.g. sep = null or "br", doublesep = null or "p"
