9c620bff7aab67a7c0443d3001546ae82eaec83a
pzinn <pzinnjustin@gmail.com>
2021-12-22 17:06:30 +1100

    temp

--- src/server/server.ts
+++ src/server/server.ts
@@ -91,6 +91,7 @@ const getInstance = function (client: Client, next) {
   if (client.instance) {
     instanceManager.checkInstance(client.instance, function (error) {
       if (error) {
+        logger.warn("instance error: " + error, client); // TEMP
         systemChat(
           client,
           "A new version of Macaulay2 is available. Type /kill in chat to upgrade."
