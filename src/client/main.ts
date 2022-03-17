deaa023415fce41f9d8cd1a000a31c9bf22ce36e
pzinn <pzinnjustin@gmail.com>
2022-03-18 00:40:30 +1100

    data-m2code to allow pasting of M2 code from arbitrary html element

--- src/client/main.ts
+++ src/client/main.ts
@@ -112,7 +112,8 @@ const clickAction = function (e) {
     }
     if (
       ((t.tagName == "CODE" && language(t) == "Macaulay2") ||
-        t.classList.contains("M2PastInput")) &&
+       t.dataset.m2code // allows to emulate code pasting from arbitrary html element
+       || t.classList.contains("M2PastInput")) &&
       t.ownerDocument.getSelection().isCollapsed
     ) {
       e.stopPropagation();
