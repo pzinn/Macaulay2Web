0f7cf34bcb8ee7195085a0c7dca68dc505df75a7
pzinn <pzinnjustin@gmail.com>
2024-01-29 21:42:22 +1100

    fix getCaret bug, getCaret2 rewrite

--- src/client/htmlTools.ts
+++ src/client/htmlTools.ts
@@ -25,66 +25,41 @@ const baselinePosition = function (el) {
 
 // TODO: rewrite getCaret(2) in a similar way as setCaret
 // caret (always assuming selection is collapsed)
+const getCaretInternal = function(el,node,offset): number | null {
+  let cur = el;
+  let len = 0;
+  while (true) {
+    if (cur === node) {
+      if (cur.nodeType === 3 || offset === 0)
+        // bingo
+        return len + offset;
+      // more complicated: target node is an element
+      node = node.childNodes[offset];
+      offset = 0;
+    }
+    if (cur.nodeType === 3)
+      // Text node
+      len += cur.textContent.length;
+    if (cur.nodeType !== 1 || (cur.nodeType === 1 && !cur.firstChild)) {
+      if (cur == el) return null;
+      // backtrack
+      while (!cur.nextSibling) {
+        if (cur.nodeName == "DIV" || cur.nodeName == "BR") len++; // for Firefox
+        cur = cur.parentElement;
+        if (cur == el) return null;
+      }
+      if (cur.nodeName == "DIV" || cur.nodeName == "BR") len++; // for Firefox
+      cur = cur.nextSibling;
+    } else cur = cur.firstChild; // forward
+  }
+}
 const getCaret = function (el): number | null {
-  const sel = window.getSelection();
-  if (el === sel.focusNode) return sel.focusOffset;
-  let cur = el.firstChild;
-  if (!cur) return null;
-  let len = 0;
-  while (true) {
-    if (cur == sel.focusNode)
-      // bingo
-      return len + sel.focusOffset;
-    if (cur.nodeType === 3)
-      // Text node
-      len += cur.textContent.length;
-    if (cur.nodeType !== 1 || (cur.nodeType === 1 && !cur.firstChild)) {
-      // backtrack
-      while (!cur.nextSibling) {
-        if (cur.nodeName == "DIV" || cur.nodeName == "BR") len++; // for Firefox
-        cur = cur.parentElement;
-        if (cur == el) return null;
-      }
-      if (cur.nodeName == "DIV" || cur.nodeName == "BR") len++; // for Firefox
-      cur = cur.nextSibling;
-    } else cur = cur.firstChild; // forward
-  }
+    const sel = window.getSelection();
+    return getCaretInternal(el,sel.focusNode,sel.focusOffset);
 };
-
 const getCaret2 = function (el) {
-  // gives both start and end of selection -- may be in wrong order!
-  const sel = window.getSelection();
-  let anchorPos, focusPos;
-  if (el === sel.anchorNode) anchorPos = sel.anchorOffset;
-  if (el === sel.focusNode) focusPos = sel.focusOffset;
-  if (anchorPos !== undefined && focusPos !== undefined)
-    return [anchorPos, focusPos];
-  let cur = el.firstChild;
-  if (!cur) return null;
-  let len = 0;
-  while (true) {
-    if (cur == sel.anchorNode) {
-      anchorPos = len + sel.anchorOffset;
-      if (focusPos !== undefined) return [anchorPos, focusPos];
-    }
-    if (cur == sel.focusNode) {
-      focusPos = len + sel.focusOffset;
-      if (anchorPos !== undefined) return [anchorPos, focusPos];
-    }
-    if (cur.nodeType === 3)
-      // Text node
-      len += cur.textContent.length;
-    if (cur.nodeType !== 1 || (cur.nodeType === 1 && !cur.firstChild)) {
-      // backtrack
-      while (!cur.nextSibling) {
-        if (cur.nodeName == "DIV" || cur.nodeName == "BR") len++; // for Firefox
-        cur = cur.parentElement;
-        if (cur == el) return null;
-      }
-      if (cur.nodeName == "DIV" || cur.nodeName == "BR") len++; // for Firefox
-      cur = cur.nextSibling;
-    } else cur = cur.firstChild; // forward
-  }
+    const sel = window.getSelection();
+    return [getCaretInternal(el,sel.anchorNode,sel.anchorOffset),getCaretInternal(el,sel.focusNode,sel.focusOffset)];
 };
 
 const utf8 = new TextEncoder(); // M2 uses utf8, counts locations in bytes :/
